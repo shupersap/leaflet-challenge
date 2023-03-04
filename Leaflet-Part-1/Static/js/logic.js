25a0d4676a2ec2deafdd50ca085aa39e42991751
shupersap <shupersap@gmail.com>
2023-03-03 23:24:45 -0500

    updated

--- Leaflet-Part-1/Static/js/logic.js
+++ Leaflet-Part-1/Static/js/logic.js
@@ -1,71 +1,58 @@
-//url for the earthquake data
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-// Create a new map object
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
 var map = L.map('map').setView([37.8, -96], 4);
 
-// Add a base layer to the map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
     maxZoom: 18,
-    
 }).addTo(map);
 
-d3.json(url).then(function (data){
-    
+d3.json(url).then(function (data) {
     L.geoJSON(data, {
-        pointToLayer: function(feature, latlng) {
-
-            
-            // create a marker for each earthquake
+        pointToLayer: function (feature, latlng) {
             var marker = L.circleMarker(latlng, {
-                radius: feature.properties.mag * 5, // adjust the size of the marker based on the magnitude
-                color: getColor(feature.geometry.coordinates[2]), // set the color of the marker based on the depth
+                radius: feature.properties.mag * 5,
+                fillColor: getColor(feature.geometry.coordinates[2]),
+                color: "#000",
                 weight: 1,
+                opacity: 1,
                 fillOpacity: 0.8
-                });
-                return marker;
-                },
-                onEachFeature: function(feature, layer) {
-                // add a popup with information about the earthquake to each marker
-                layer.bindPopup("<b>Magnitude:</b> " + feature.properties.mag + "<br><b>Location:</b> " + feature.properties.place+ "<br><b>Depth:</b> "+ feature.geometry.coordinates[2]);
-                }
-                }).addTo(map);
-                });
-        function getColor(depth) {
-                    if (depth < 10) {
-                    return '#00FF00'; // light green
-                    } else if (depth < 50) {
-                    return '#FFFF00'; // yellow
-                    } else if (depth < 100) {
-                    return '#FFA500'; // orange
-                    } else if (depth < 200) {
-                    return '#FF0000'; // red
-                    } else {
-                    return '#8B008B'; // dark purple
-                    }
-                    }
-//create legend
-var legend = L.control({position: 'bottomright'});
+            });
+            return marker;
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("<b>Magnitude:</b> " + feature.properties.mag + "<br><b>Location:</b> " + feature.properties.place + "<br><b>Depth:</b> " + feature.geometry.coordinates[2]);
+        }
+    }).addTo(map);
 
-legend.onAdd = function (map) {
-    var div = L.DomUtil.create('div', 'info legend');
-    var colors = ['#00FF00', '#FFFF00', '#FFA500', '#FF0000', '#8B008B'];
-    var labels = ['<10', '10-50', '50-100', '100-200', '>200'];
+    // Add legend
+    var legend = L.control({position: 'bottomright'});
 
-    // loop through the colors and labels and add a legend item for each
-    for (var i = 0; i < colors.length; i++) {
-        // create a legend item
-        var legendItem = L.DomUtil.create('i', 'legend-item');
-        legendItem.style.backgroundColor = colors[i];
-        legendItem.innerHTML = labels[i];
+    legend.onAdd = function (map) {
 
-        // add the legend item to the div
-        div.appendChild(legendItem);
-    }
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [-10, 10, 30, 50, 70, 90],
+            labels = [];
 
-    return div;
+        // loop through depth intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(map);
+
+});
+
+function getColor(depth) {
+    var minDepth = -10;
+    var maxDepth = 90;
+    var colorScale = d3.scaleLinear()
+        .domain([minDepth, maxDepth])
+        .range(['#00FF00', '#FF0000']);
+    return colorScale(depth);
 };
-
-legend.addTo(map);
-
-                    
